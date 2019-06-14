d5058c9a3c04de29772460fbc8d88174762c59ac
emilebaelde <emilebaelde@hotmail.com>
2019-06-14 18:43:48 +0200

    quick fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,10 @@
 
 @section('content')
     <div class="content mb-5">
-
+        @include('includes.search',['url'=>''])
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    @include('includes.search',['url'=>''])
                     @if($stocks)
                         @foreach($stocks as $stock)
 
